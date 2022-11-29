32fe792385cbfd7c35da52652fdfb50933ef2f39
luis-godinez <beantek@gmail.com>
2022-11-29 14:28:24 -0800

    Update rules and keymap. Resolve compile errors.

--- keyboards/ungodly/launch_pad/keymaps/MW2/keymap.c
+++ keyboards/ungodly/launch_pad/keymaps/MW2/keymap.c
@@ -24,20 +24,78 @@ void matrix_init_user (void) {
   }
 }
 
-// Tap Dance declarations
+// Advanced Tap Dance
+
+// Keycode declarations
 enum {
-    TD_ESC_TAB,
-    TD_3_L0,
-    TD_NUMLOCK_L1,
+    TD_3_5,
+    TD_4_6,
+    ALT_L1_TOGGLE,
+    NUMLOCK_L1_TOGGLE,
 };
 
-// Tap Dance definitions
+typedef struct {
+    uint16_t tap;
+    uint16_t hold;
+    uint16_t held;
+} tap_dance_tap_hold_t;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    qk_tap_dance_action_t *action;
+
+    switch (keycode) {
+        case TD(TD_3_5):
+        case TD(TD_4_6):
+            action = &tap_dance_actions[TD_INDEX(keycode)];
+            if (!record->event.pressed && action->state.count && !action->state.finished) {
+                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+                tap_code16(tap_hold->tap);
+            }
+    }
+    return true;
+}
+
+void tap_dance_tap_hold_finished(qk_tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (state->pressed) {
+        if (state->count == 1
+#ifndef PERMISSIVE_HOLD
+            && !state->interrupted
+#endif
+        ) {
+            register_code16(tap_hold->hold);
+            tap_hold->held = tap_hold->hold;
+        } else {
+            register_code16(tap_hold->tap);
+            tap_hold->held = tap_hold->tap;
+        }
+    }
+}
+
+void tap_dance_tap_hold_reset(qk_tap_dance_state_t *state, void *user_data) {
+    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
+
+    if (tap_hold->held) {
+        unregister_code16(tap_hold->held);
+        tap_hold->held = 0;
+    }
+}
+
+#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
+    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
+
+
 qk_tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Escape, twice for Number 4 (armor plates in warzone)
-    [TD_ESC_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_TAB),
-    [TD_3_L0] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_3, 1),
-    [TD_NUMLOCK_L1] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NUMLOCK, 1),
+    [TD_3_5] = ACTION_TAP_DANCE_TAP_HOLD(KC_3, KC_5),
+    [TD_4_6] = ACTION_TAP_DANCE_TAP_HOLD(KC_4, KC_6),
+    [ALT_L1_TOGGLE] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_LALT, 1),
+    [NUMLOCK_L1_TOGGLE] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NUMLOCK, 1),
 };
+
+// LCTL(LALT(KC_TAB))
+
+// Layers
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap _BL: (Base Layer) Default Numpad Layer
@@ -54,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * `--------------------'
   */
   [0] = LAYOUT_ortho_5x4(
-   TD(TD_NUMLOCK_L1),  KC_PSLS,  KC_PAST, LT(2, KC_BSPC),
+   TD(NUMLOCK_L1_TOGGLE),  KC_PSLS,  KC_PAST, LT(2, KC_BSPC),
    KC_P7,    KC_P8,    KC_P9,        KC_PMNS,
    KC_P4,    KC_P5,    KC_P6,        KC_PPLS,
    KC_P1,    KC_P2,    KC_P3,        KC_PENT,
@@ -75,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */
   [1] = LAYOUT_ortho_5x4(
     KC_R,    KC_F,    KC_V,     KC_SPACE,
-    KC_E,    KC_D,    KC_C,     KC_LALT,
-    KC_W,    KC_S,    KC_X,     KC_4,
-    KC_Q,    KC_A,    KC_Z,     TD(TD_3_L0),
-    TD(TD_ESC_TAB),    KC_LCTRL,  KC_LSHIFT,   KC_M
+    KC_E,    KC_D,    KC_C,     TD(ALT_L1_TOGGLE),
+    KC_W,    KC_S,    KC_X,     TD(TD_4_6),
+    KC_Q,    KC_A,    KC_Z,     TD(TD_3_5),
+    KC_ESC, KC_LCTRL, KC_LSHIFT, KC_M
   ),
   /* Keymap _FN: RGB Function Layer
    * ,-------------------.
