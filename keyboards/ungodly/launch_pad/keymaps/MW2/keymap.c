4840ce479304b1cc50e6d35cd8d69b595dbbe0a3
luis-godinez <beantek@gmail.com>
2022-11-29 15:40:34 -0800

    Update MW2 keymap. Gave up on tap-hold dance dance due to TD_INDEX error

--- keyboards/ungodly/launch_pad/keymaps/MW2/keymap.c
+++ keyboards/ungodly/launch_pad/keymaps/MW2/keymap.c
@@ -28,69 +28,17 @@ void matrix_init_user (void) {
 
 // Keycode declarations
 enum {
-    TD_3_5,
-    TD_4_6,
     ALT_L1_TOGGLE,
     NUMLOCK_L1_TOGGLE,
+    TD_4_6,
+    TD_3_5,
 };
 
-typedef struct {
-    uint16_t tap;
-    uint16_t hold;
-    uint16_t held;
-} tap_dance_tap_hold_t;
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    qk_tap_dance_action_t *action;
-
-    switch (keycode) {
-        case TD(TD_3_5):
-        case TD(TD_4_6):
-            action = &tap_dance_actions[TD_INDEX(keycode)];
-            if (!record->event.pressed && action->state.count && !action->state.finished) {
-                tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
-                tap_code16(tap_hold->tap);
-            }
-    }
-    return true;
-}
-
-void tap_dance_tap_hold_finished(qk_tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (state->pressed) {
-        if (state->count == 1
-#ifndef PERMISSIVE_HOLD
-            && !state->interrupted
-#endif
-        ) {
-            register_code16(tap_hold->hold);
-            tap_hold->held = tap_hold->hold;
-        } else {
-            register_code16(tap_hold->tap);
-            tap_hold->held = tap_hold->tap;
-        }
-    }
-}
-
-void tap_dance_tap_hold_reset(qk_tap_dance_state_t *state, void *user_data) {
-    tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)user_data;
-
-    if (tap_hold->held) {
-        unregister_code16(tap_hold->held);
-        tap_hold->held = 0;
-    }
-}
-
-#define ACTION_TAP_DANCE_TAP_HOLD(tap, hold) \
-    { .fn = {NULL, tap_dance_tap_hold_finished, tap_dance_tap_hold_reset}, .user_data = (void *)&((tap_dance_tap_hold_t){tap, hold, 0}), }
-
-
 qk_tap_dance_action_t tap_dance_actions[] = {
-    [TD_3_5] = ACTION_TAP_DANCE_TAP_HOLD(KC_3, KC_5),
-    [TD_4_6] = ACTION_TAP_DANCE_TAP_HOLD(KC_4, KC_6),
     [ALT_L1_TOGGLE] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_LALT, 1),
     [NUMLOCK_L1_TOGGLE] = ACTION_TAP_DANCE_LAYER_TOGGLE(KC_NUMLOCK, 1),
+    [TD_4_6] = ACTION_TAP_DANCE_DOUBLE(KC_4, KC_6),
+    [TD_3_5] = ACTION_TAP_DANCE_DOUBLE(KC_3, KC_5),
 };
 
 // LCTL(LALT(KC_TAB))
