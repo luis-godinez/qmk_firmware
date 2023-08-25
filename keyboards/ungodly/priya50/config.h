51aefe98e1f77fbfe1be90996462cdbd08da7796
Luis Godinez <beantek@gmail.com>
2023-08-25 10:14:00 -0700

    Initial commit of working firmware. OLED needs to be programmed.

---
+++ keyboards/ungodly/priya50/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2022 DZTECH <moyi4681@live.cn>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#    define RGBLED_NUM 8
+
+
+/* settings for the oled */
+#define I2C1_SDA_PIN GP30
+#define I2C1_SCL_PIN GP31
+#define I2C_DRIVER I2CD1
+
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 200U // Timeout window in ms in which the double tap can occur.
+#define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP26 // Specify a optional status led by GPIO number which blinks when entering the bootloader
