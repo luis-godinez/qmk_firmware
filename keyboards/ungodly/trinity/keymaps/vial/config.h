5291e7c0b9d693cace2db752e394dd7fc89ac418
Luis Godinez <ungodly@Luiss-MacBook-Pro.local>
2024-03-30 14:49:05 -0700

    Add Trinity keyboard
    Add firmware generated using zykrah/firmware-scripts. Not tested yet.

---
+++ keyboards/ungodly/trinity/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xFD, 0x32, 0x67, 0x92, 0xFB, 0x5C, 0xA4, 0x90}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
